fce96e144c5143bb8d0c532889934d3b236919ed
Anton Bobrov <bobrov@vl.ru>
2011-04-18 09:47:10 +1100

    skip slow test only on mass test run

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,5 +2,6 @@ import pytest
 import os
 
 def pytest_runtest_setup(item):
-    if 'slow' in item.keywords and 'SKIP_SLOW_TESTS' in os.environ:
+    if 'slow' in item.keywords and 'SKIP_SLOW_TESTS' in os.environ \
+            and len(item.session._collected) > 1:
         pytest.skip("ommit SKIP_SLOW_TESTS envvar to run")

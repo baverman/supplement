42d313a143ef607bd1829366f743fd5c9099a443
Anton Bobrov <bobrov@vl.ru>
2011-04-18 15:38:55 +1100

    more detailed test for watcher

--- tests/test_watcher.py
+++ tests/test_watcher.py
@@ -8,11 +8,15 @@ def test_watcher_must_call_uniq_handlers_on_file_change(tmpdir):
     f = tmpdir.join('test.py')
     f.write('content')
 
+    # Wrap list with hashable object
     class Holder(object):
         def __init__(self):
             self.data = []
 
     def on_change(fname, l):
+        l.data.append(fname)
+
+    def change_on(fname, l):
         l.data.append(fname)
 
     changed1 = Holder()
@@ -21,9 +25,10 @@ def test_watcher_must_call_uniq_handlers_on_file_change(tmpdir):
     watcher.monitor(str(f), on_change, changed1)
     watcher.monitor(str(f), on_change, changed1)
     watcher.monitor(str(f), on_change, changed2)
+    watcher.monitor(str(f), change_on, changed2)
 
     f.write('ddddddd')
 
     time.sleep(2)
-    assert changed1.data== [str(f)]
-    assert changed2.data == [str(f)]
+    assert changed1.data == [str(f)]
+    assert changed2.data == [str(f), str(f)]

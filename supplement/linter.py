a8ac1567097d288d83aabb6e0410721194a2f4c9
Anton Bobrov <bobrov@vl.ru>
2011-11-12 01:02:02 +1100

    linter

--- supplement/linter.py
+++ supplement/linter.py
@@ -1,17 +1,356 @@
-import ast
+from bisect import bisect_left as bisect
+from ast import NodeVisitor, Load, parse, Name as AstName
+from tokenize import NL, NEWLINE, ERRORTOKEN, INDENT, DEDENT, generate_tokens, TokenError, NAME
+from keyword import iskeyword
+from contextlib import contextmanager
+
 from fixer import try_to_fix
 
-def lint(code, syntax_only=False):
+def check_syntax(source):
     try:
-        tree = ast.parse(code)
+        parse(source)
     except Exception, e:
-        code, fixed_location = try_to_fix(e, code)
+        _, fixed_location = try_to_fix(e, source)
         if fixed_location:
-            return [('syntax-error', e.msg, fixed_location)]
+            return fixed_location, e.msg
         else:
-            return [('syntax-error', e.msg, ('end-of-line', e.lineno))]
+            return ('end-of-line', e.lineno), e.msg
     else:
-        if syntax_only:
-            return []
+        return None
+
+def lint(source):
+    tree = parse(source)
+    result = []
+    result.extend(check_names(source, tree))
+    return result
+
+def check_names(source, tree):
+    result = []
+    usages, main_scope = NameExtractor().process(tree, IdxNameExtractor(source))
+
+    for scope, name, line, offset in usages:
+        n = scope.get_name(name, line, offset)
+        if not n:
+            result.append(((line, offset), name, 'Unknown name (E01): ' + name))
         else:
-            raise NotImplementedError()
+            n.usages.append((line, offset))
+
+    for name in main_scope.get_names():
+        if not name.is_used() and not name.name.startswith('_'):
+            result.append(((name.line, name.offset), name.name, 'Unused name (W01): ' + name.name))
+
+    return result
+
+
+class Name(object):
+    def __init__(self, name, line, offset, indirect_use=False):
+        self.name = name
+        self.line = line
+        self.offset = offset
+        self.usages = []
+        self.indirect_use = indirect_use
+        self.declared_at_loop = None
+
+    def is_used(self):
+        if self.indirect_use or self.usages:
+            return True
+
+        if self.declared_at_loop:
+            start, end = self.declared_at_loop
+            for _, _, name in self.scope.names.get(self.name, []):
+                if any(end > r >= start for r in name.usages):
+                    return True
+
+        return False
+
+    def __repr__(self):
+        return "Name(%s, %s, %s, %s, %s)" % (self.name, self.line, self.offset,
+            self.indirect_use, self.declared_at_loop)
+
+class BuiltinScope(object):
+    def __init__(self):
+        self.names = {}
+        self.childs = []
+
+    def get_name(self, name):
+        try:
+            return self.names[name]
+        except KeyError:
+            pass
+
+        if name == '__builtins__' or name in __builtins__:
+            result = Name(name, 0, 0, True)
+        else:
+            result = None
+
+        self.names[name] = result
+        return result
+
+class Scope(object):
+    def __init__(self, parent=None, is_block=True, passthrough=False):
+        self.names = {}
+        self.parent = parent
+        self.is_block = is_block
+        self.childs = []
+        self.passthrough = passthrough
+
+        if parent:
+           parent.childs.append(self)
+
+    def add_name(self, name, starts=None):
+        if starts:
+            value = starts[0], starts[1], name
+        else:
+            value = name.line, name.offset, name
+        self.names.setdefault(name.name, []).append(value)
+        name.scope = self
+        return name
+
+    def get_name(self, name, line=None, offset=None):
+        if name in self.names:
+            nnames = self.names[name]
+            if line is not None and self.is_block:
+                value = line, offset, None
+                idx = bisect(nnames, value) - 1
+                if idx >= 0:
+                    return nnames[idx][2]
+            elif not self.passthrough:
+                return nnames[-1][2]
+
+        if self.parent:
+            return self.parent.get_name(name)
+
+        return None
+
+    def get_names(self):
+        for nnames in self.names.itervalues():
+            for _, _, name in nnames:
+                yield name
+
+        for c in self.childs:
+            for name in c.get_names():
+                yield name
+
+class GetExprEnd(NodeVisitor):
+    def __call__(self, node):
+        self.visit(node)
+        return self.last_loc
+
+    def visit_Name(self, node):
+        self.last_loc = node.lineno, node.col_offset + len(node.id)
+
+class NameExtractor(NodeVisitor):
+    def process(self, root, idx_name_extractor):
+        self.idx_name_extractor = idx_name_extractor
+        self.scope = self.main_scope = Scope(BuiltinScope())
+        self.usages = []
+        self.indirect_use = False
+        self.effect_starts = None
+        self.get_expr_end = GetExprEnd()
+        self.declared_at_loop = False
+
+        self.generic_visit(root)
+
+        return self.usages, self.main_scope
+
+    @contextmanager
+    def indirect(self, is_indirect):
+        old_indirect = self.indirect_use
+        self.indirect_use = is_indirect
+        yield None
+        self.indirect_use = old_indirect
+
+    @contextmanager
+    def effect(self, starts_at):
+        oldstate = self.effect_starts
+        self.effect_starts = starts_at
+        yield None
+        self.effect_starts = oldstate
+
+    @contextmanager
+    def loop(self, start=None, end=None):
+        oldstate = self.declared_at_loop
+        if start:
+            self.declared_at_loop = start, end
+        else:
+            self.declared_at_loop = None
+        yield None
+        self.declared_at_loop = oldstate
+
+    def visit_Import(self, node):
+        idx = 0
+        for n in node.names:
+            if n.asname:
+                name = n.asname
+                idx += n.name.count('.') + 1
+            else:
+                name = n.name
+
+            line, offset = self.idx_name_extractor.get(node.lineno, idx)
+            self.scope.add_name(Name(name.partition('.')[0], line, offset))
+            idx += name.count('.') + 1
+
+    def visit_ImportFrom(self, node):
+        idx = 0
+        if node.module:
+            idx += node.module.count('.') + 1
+
+        for n in node.names:
+            if n.name == '*':
+                continue
+
+            if n.asname:
+                name = n.asname
+                idx += n.name.count('.') + 1
+            else:
+                name = n.name
+
+            line, offset = self.idx_name_extractor.get(node.lineno, idx)
+            self.scope.add_name(Name(name, line, offset))
+            idx += name.count('.') + 1
+
+    def visit_Assign(self, node):
+        with self.effect(self.get_expr_end(node)):
+            if self.is_main_scope():
+                with self.indirect(True):
+                    self.generic_visit(node)
+            else:
+                self.generic_visit(node)
+
+    def visit_AugAssign(self, node):
+        if type(node.target) is AstName:
+            self.usages.append((self.scope, node.target.id, node.target.lineno, node.target.col_offset))
+
+        self.visit_Assign(node)
+
+    def visit_Name(self, node):
+        if type(node.ctx) == Load:
+            self.usages.append((self.scope, node.id, node.lineno, node.col_offset))
+        else:
+            name = self.scope.add_name(Name(node.id, node.lineno, node.col_offset, self.indirect_use),
+                self.effect_starts)
+
+            if self.declared_at_loop:
+                name.declared_at_loop = self.declared_at_loop
+
+    def visit_FunctionDef(self, node):
+        line, offset = self.idx_name_extractor.get(node.lineno, 0)
+        self.scope.add_name(Name(node.name, line, offset,
+            self.indirect_use or self.is_main_scope()))
+        self.scope = Scope(self.scope)
+        self.scope.lineno = line
+        self.scope.offset = offset
+        with self.loop():
+            with self.indirect(False):
+                self.generic_visit(node)
+        self.scope = self.scope.parent
+
+    def visit_ClassDef(self, node):
+        line, offset = self.idx_name_extractor.get(node.lineno, 0)
+        self.scope.add_name(Name(node.name, line, offset,
+            self.indirect_use or self.is_main_scope()))
+        self.scope = Scope(self.scope, passthrough=True)
+        self.scope.lineno = line
+        self.scope.offset = offset
+        with self.loop():
+            with self.indirect(True):
+                self.generic_visit(node)
+        self.scope = self.scope.parent
+
+    def visit_GeneratorExp(self, node):
+        with self.effect((node.lineno, node.col_offset-1)):
+            self.generic_visit(node)
+
+    def visit_ListComp(self, node):
+        self.visit_GeneratorExp(node)
+
+    def visit_For(self, node):
+        start = node.body[0]
+        with self.loop((start.lineno, start.col_offset), self.get_expr_end(node)):
+            self.generic_visit(node)
+
+    def visit_While(self, node):
+        with self.loop((node.lineno, node.col_offset), self.get_expr_end(node)):
+            self.generic_visit(node)
+
+    def visit_arguments(self, node):
+        if node.vararg:
+            self.scope.add_name(Name(node.vararg, self.scope.lineno, self.scope.offset, True))
+
+        if node.kwarg:
+            self.scope.add_name(Name(node.kwarg, self.scope.lineno, self.scope.offset, True))
+
+        with self.indirect(True):
+            self.generic_visit(node)
+
+    def is_main_scope(self):
+        return self.scope is self.main_scope
+
+class TokenGenerator(object):
+    def __init__(self, lines):
+        it = iter(lines)
+        self.tokens = generate_tokens(it.next)
+        self.onhold = None
+
+    def get(self, *tids):
+        tok = self.next()
+        if tok[0] not in tids and tok[1] not in tids:
+            raise Exception('(%s,%s) not in %s' % (tok[:2], tids))
+
+        return tok
+
+    def hold(self, tok):
+        self.onhold = tok
+
+    def skip(self, *tids):
+        tok = self.next()
+        if tok[0] not in tids and tok[1] not in tids:
+            self.onhold = tok
+            return False
+
+        return True
+
+    def skipmany(self, *tids):
+        while self.skip(*tids):
+            pass
+        else:
+            return False
+
+        return True
+
+    SPACES = set((NL, NEWLINE, ERRORTOKEN, INDENT, DEDENT))
+    def next(self):
+        if self.onhold:
+            tid, value, start, end, _ = self.onhold
+            self.onhold = None
+        else:
+            try:
+                tid = NL
+                while tid in self.SPACES:
+                    tid, value, start, end, _  = self.tokens.next()
+            except (TokenError, StopIteration):
+                tid, value, start, end = 0, '', (0,0), (0,0)
+
+        return tid, value, start, end
+
+    def __iter__(self):
+        while True:
+            tok = self.next()
+            if not tok[0]:
+                break
+
+            yield tok
+
+class IdxNameExtractor(object):
+    def __init__(self, source):
+        self.lines = source.splitlines()
+
+    def get(self, lineno, idx):
+        i = -1
+        for tid, value, start, _ in TokenGenerator(self.lines[lineno-1:]):
+            if tid == NAME and not iskeyword(value):
+                i += 1
+                if i == idx:
+                    return start[0] + lineno - 1, start[1]
+
+        return lineno, 0
---
+++ tests/test_linter.py
@@ -0,0 +1,142 @@
+import re
+from supplement.linter import lint
+from .helpers import cleantabs
+
+def get_source_and_expected_result(source):
+    source = cleantabs(source)
+    result = []
+    while True:
+        m = re.search(r'([$!])(.+?)\1', source)
+        if not m: break
+
+        lineno = source.count('\n', 0, m.start()) + 1
+        offset = m.start() - len(''.join(source.splitlines(True)[:lineno-1]))
+
+        c = m.group(1)
+        if c == '$':
+            message = 'Unused name (W01): ' + m.group(2)
+        else:
+            message = 'Unknown name (E01): ' + m.group(2)
+        result.append(((lineno, offset), m.group(2), message))
+        source= source.replace(c, '', 2)
+
+    return source, result
+
+def assert_names(source):
+    source, expected = get_source_and_expected_result(source)
+    result = lint(source)
+    assert set(result) == set(expected)
+
+def test_module_level_variables_must_be_skipped_as_unused():
+    assert_names('''
+        var = 1
+    ''')
+
+def test_unused_imports():
+    assert_names('''
+        import $package1$.module, package1.module as $modulo$
+
+        from ..package import *
+        from .package.module import $boo$, $foo$
+        from . import $bar$
+    ''')
+
+def test_module_level_unused_names():
+    assert_names('''
+        for $a$, b in []:
+            b
+    ''')
+
+def test_underscore_names():
+    assert_names('''
+        for _, _test in []:
+            pass
+    ''')
+
+def test_unknown_names():
+    assert_names('''
+        !name!
+    ''')
+
+def test_builtin_names():
+    assert_names('''
+        True
+        None
+        __builtins__
+        Exception
+    ''')
+
+def test_func_and_arg_names():
+    assert_names('''
+        def boo(bar):
+            def $bar$():
+                pass
+
+        def foo(arg, *args, **kwargs):
+            !bar!
+            boo()
+            args
+            kwargs
+
+        foo()
+    ''')
+
+def test_class_defenitions():
+    assert_names('''
+        class Boo(object):
+            def boo(self):
+                pass
+
+            class Foo(object): pass
+
+            def foo(self):
+                class $Bar$(object):
+                    pass
+        Boo()
+    ''')
+
+def test_declaration_action_scope():
+    assert_names('''
+        def bar():
+            boo = 1
+            $boo$ = boo + 1
+
+            $foo$ = !foo! + 1
+    ''')
+
+def test_self_changed_names():
+    assert_names('''
+        def bar():
+            boo = 1
+            $boo$ += 1
+    ''')
+
+def test_looped_name_usage_in_for_statements():
+    assert_names('''
+        def bar():
+            for i in []:
+                i += 1
+
+            found = False
+            while not found:
+                found, $boo$ = True, False
+
+            boo = 1
+            return boo
+    ''')
+
+def test_generator_and_list_expressions():
+    assert_names('''
+        def bar():
+            (r for r in (1,2,3))
+            [rr for rr in (1,2,3)]
+    ''')
+
+def test_class_scope_must_not_hide_global_names():
+    assert_names('''
+        from . import bar
+
+        class Bar(object):
+            def bar(self):
+                return bar()
+    ''')

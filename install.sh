ecc1d1670ef317809cfb082d4165f920d9a64431
Anton Bobrov <bobrov@vl.ru>
2012-01-09 14:33:22 +1100

    ./install.sh in py3 branch

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #/bin/sh
 
-python setup.py sdist
-sudo pip install -U --no-deps dist/`python setup.py --fullname`.tar.gz
+python3 setup.py sdist
+sudo pip-3.2 install -U --no-deps dist/`python3 setup.py --fullname`.tar.gz
